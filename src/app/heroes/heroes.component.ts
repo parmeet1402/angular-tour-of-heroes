fad23ce5cc30deee7c49059209a0394d7b679a82
Parmeet <parmeet.asi@gmail.com>
2019-02-24 01:37:25 +0530

    divided code into 2 components having parent child relationship

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,12 +8,17 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   }
   constructor() { }
 
+  selectedHero: Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero= hero;
+  }
   ngOnInit() {
   }
 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Parmeet'},
+    {id: 12, name: 'Ekampreet'},
+    {id: 13, name: 'Davinder'},
+    {id: 14, name: 'Avi Asija'},
+    {id: 15, name: 'Ekam'},
+    {id: 16, name: 'Ricky'},
+    {id: 17, name: 'Rhea'},
+    {id: 18, name: 'Danish'},
+    {id: 19, name: 'Ankit'},
+    {id: 20, name: 'Kritika'}
+]
